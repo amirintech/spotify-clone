289d384dcacaf481b5239c97d12964869ff7fc5a
Amir <91920799+amirintech@users.noreply.github.com>
2025-04-17 15:57:58 +0200

    Update authorized image domains

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['irpkrjsfjcwkfxytivrm.supabase.co'],
+    domains: ['huknjvkasrtwswxodpwk.supabase.co'],
   },
 }
 
